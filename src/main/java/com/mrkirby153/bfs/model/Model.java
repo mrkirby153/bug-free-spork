4281e5ae446e9e76d008d4994a8c8b29b54defdb
mrkirby153 <mr.austinwhyte@gmail.com>
2018-04-04 20:36:14 -0700

    Add connection factory to the model

--- src/main/java/com/mrkirby153/bfs/model/Model.java
+++ src/main/java/com/mrkirby153/bfs/model/Model.java
@@ -1,7 +1,25 @@
 package com.mrkirby153.bfs.model;
 
+import com.mrkirby153.bfs.ConnectionFactory;
+
 /**
  * A model in the database
  */
 public class Model {
+
+    /**
+     * The connection factory used for the database
+     */
+    private static ConnectionFactory connectionFactory;
+
+
+    /**
+     * Sets the connection factory used globally
+     *
+     * @param factory The connection factory
+     */
+    public static void setConnectionFactory(ConnectionFactory factory) {
+        connectionFactory = factory;
+    }
+
 }
