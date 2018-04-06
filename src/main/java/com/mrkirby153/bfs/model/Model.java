b3c342d7a89f6db944c1ee8a4a4e8403fc95dcc2
mrkirby153 <mr.austinwhyte@gmail.com>
2018-04-06 16:03:04 -0700

    Add options for querying models

--- src/main/java/com/mrkirby153/bfs/model/Model.java
+++ src/main/java/com/mrkirby153/bfs/model/Model.java
@@ -1,12 +1,14 @@
 package com.mrkirby153.bfs.model;
 
-import com.mrkirby153.bfs.ConnectionFactory;
 import com.mrkirby153.bfs.annotations.Column;
 import com.mrkirby153.bfs.annotations.PrimaryKey;
 import com.mrkirby153.bfs.annotations.Table;
+import com.mrkirby153.bfs.sql.QueryBuilder;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,10 +21,6 @@ import java.util.Optional;
  */
 public class Model {
 
-    /**
-     * The connection factory used for the database
-     */
-    private static ConnectionFactory connectionFactory;
     @Column("created_at")
     public Timestamp createdAt;
     @Column("updated_at")
@@ -33,21 +31,138 @@ public class Model {
     protected boolean timestamps = true;
 
     /**
-     * Gets the global connection factory
+     * Gets a list of all the models matching the query
      *
-     * @return The connection factory
+     * @param modelClass The model class
+     * @param column     The column to query
+     * @param operator   The comparison to perform
+     * @param data       The data to check
+     *
+     * @return A list of models matching the query or an empty array if none exist
      */
-    public static ConnectionFactory getConnectionFactory() {
-        return connectionFactory;
+    public static <T extends Model> List<T> get(Class<T> modelClass, String column, String operator, Object data) {
+        return get(modelClass, new ModelOption(column, operator, data));
     }
 
     /**
-     * Sets the connection factory used globally
+     * Gets a list of all the models matching the query
      *
-     * @param factory The connection factory
+     * @param modelClass The model class
+     * @param column     The column to query
+     * @param data       The data to check
+     *
+     * @return A list of models matching the query or an empty array if none exist
      */
-    public static void setConnectionFactory(ConnectionFactory factory) {
-        connectionFactory = factory;
+    public static <T extends Model> List<T> get(Class<T> modelClass, String column, Object data) {
+        return get(modelClass, new ModelOption(column, "=", data));
+    }
+
+    /**
+     * Gets all the elements matching the query
+     *
+     * @param modelClass The model class
+     * @param pairs      The comparisons to do
+     *
+     * @return A list of models or an empty array if none exist
+     */
+    public static <T extends Model> List<T> get(Class<T> modelClass, ModelOption... pairs) {
+        try {
+            ArrayList<T> list = new ArrayList<>();
+            T instance = modelClass.newInstance();
+            QueryBuilder builder = new QueryBuilder();
+            builder.table(instance.getTable());
+            builder.select(instance.getColumnData().keySet().toArray(new String[0]));
+            for (ModelOption option : pairs) {
+                builder.where(option.getColumn(), option.getOperator(), option.getData());
+            }
+            ResultSet rs = builder.get();
+            while (rs.next()) {
+                HashMap<String, Object> data = new HashMap<>();
+                for (String column : instance.getColumnData().keySet()) {
+                    data.put(column, rs.getObject(column));
+                }
+                T newInstance = modelClass.newInstance();
+                newInstance.setData(data);
+                list.add(newInstance);
+            }
+            return list;
+        } catch (InstantiationException | IllegalAccessException | SQLException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
+    /**
+     * Gets the first element matching the query
+     *
+     * @param modelClass The model class
+     * @param column     The column to query
+     * @param operator   The comparison to perform
+     * @param data       The data to check
+     *
+     * @return The first element matching the query or null
+     */
+    public static <T extends Model> T first(Class<T> modelClass, String column, String operator,
+        Object data) {
+        List<T> list = get(modelClass, column, operator, data);
+        if (list.size() < 1) {
+            return null;
+        }
+        return list.get(0);
+    }
+
+    /**
+     * Gets the first element matching the query
+     *
+     * @param modelClass The model class
+     * @param column     The column to query
+     * @param data       The data to check
+     *
+     * @return The first element matching the query or null
+     */
+    public static <T extends Model> T first(Class<T> modelClass, String column, Object data){
+        return first(modelClass, column, "=", data);
+    }
+
+    /**
+     * Gets the first element matching the query
+     *
+     * @param modelClass The model class
+     * @param pairs      The comparisons to do
+     *
+     * @return The first element matching the query or null
+     */
+    public static <T extends Model> T first(Class<T> modelClass, ModelOption... pairs) {
+        List<T> list = get(modelClass, pairs);
+        if (list.size() < 1) {
+            return null;
+        }
+        return list.get(0);
+    }
+
+    /**
+     * Parses a {@link ResultSet} into a model
+     *
+     * @param clazz The class
+     * @param rs    The result set
+     *
+     * @return The model or null
+     */
+    public static <T extends Model> T parse(Class<T> clazz, ResultSet rs) {
+        try {
+            T instance = clazz.newInstance();
+            if (rs.next()) {
+                HashMap<String, Object> data = new HashMap<>();
+                for (String table : instance.getColumnData().keySet()) {
+                    data.put(table, rs.getObject(table));
+                }
+                instance.setData(data);
+            }
+            return instance;
+        } catch (SQLException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     /**
@@ -116,7 +231,6 @@ public class Model {
         }
     }
 
-
     /**
      * Gets the model's table in the database
      *
@@ -124,7 +238,7 @@ public class Model {
      */
     public String getTable() {
         if (!this.getClass().isAnnotationPresent(Table.class)) {
-            throw new IllegalArgumentException("The model %s does not have an @Table annotation!");
+            throw new IllegalArgumentException(String.format("The model %s does not have an @Table annotation!", this.getClass()));
         }
         return this.getClass().getAnnotation(Table.class).value();
     }
