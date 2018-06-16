04d80c95d1fa50881dd33e628ad06e3693d9f34b
mrkirby153 <mr.austinwhyte@gmail.com>
2018-06-15 19:15:07 -0700

    Add tuple class

---
+++ src/main/java/com/mrkirby153/bfs/Tuple.java
@@ -0,0 +1,15 @@
+package com.mrkirby153.bfs;
+
+/**
+ * A Tuple
+ */
+public class Tuple<A, B> {
+
+    public final A first;
+    public final B second;
+
+    public Tuple(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+}
